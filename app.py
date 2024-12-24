ed4100a3201ffe4fc762f13b6f8ad288ebd6a2b9
Kevin Sanchez <erik18012@gmail.com>
2024-12-24 20:34:26 -0300

    Primer acercamiento SQL

--- app.py
+++ app.py
@@ -1,9 +1,21 @@
 from flask import Flask
-
+from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
 
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///voley_app_database.db"
+db = SQLAlchemy(app)
+
+class Jugador(db.Model):
+    dni = db.Column(db.Integer, primary_key=True)
+    categoria = db.Column(db.String(20), primary_key=True)
+    nya = db.Column(db.String(30), nullable=False)
+    telefono = db.Column(db.Integer, nullable=True, default="0")
+    fecha_nac = db.Column(db.Datetime, nullable=False)
+    direccion = db.Column(db.String(35),nullable=True)
+    
+    
 @app.route("/")
 def home():
     return "<h1>Hello, World!</h1>"
