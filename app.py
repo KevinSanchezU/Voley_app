842fcfc0282c0c11c852ca58d072d32f9b83f5ba
Kevin Sanchez <erik18012@gmail.com>
2024-12-29 17:16:45 -0300

    Separacion y declaracion de models

--- app.py
+++ app.py
@@ -1,21 +1,15 @@
+from datetime import datetime
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-
+from models import db, Jugador, Equipo, jugador_equipo
 
 app = Flask(__name__)
 
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///voley_app_database.db"
-db = SQLAlchemy(app)
 
-class Jugador(db.Model):
-    dni = db.Column(db.Integer, primary_key=True)
-    categoria = db.Column(db.String(20), primary_key=True)
-    nya = db.Column(db.String(30), nullable=False)
-    telefono = db.Column(db.Integer, nullable=True, default="0")
-    fecha_nac = db.Column(db.Datetime, nullable=False)
-    direccion = db.Column(db.String(35),nullable=True)
-    
-    
+db.init_app(app)
+
+
 @app.route("/")
 def home():
     return "<h1>Hello, World!</h1>"
