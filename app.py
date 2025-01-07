e77d5070aea48d1b2251ddf3ea205d40cd07dc2f
Kevin Sanchez <erik18012@gmail.com>
2025-01-07 02:03:43 -0300

    terminando configuraciones

--- app.py
+++ app.py
@@ -1,14 +1,6 @@
-from datetime import datetime
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
 from website import crear_app
-from website.models import db, Jugador, Equipo, jugador_equipo
 
 app = crear_app()
-
-#app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///voley_app_database.db"
-
-#db.init_app(app)
 
 if __name__ == "__main__":
     app.run(debug=True)
