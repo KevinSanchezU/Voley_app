974d9d6b861c543757e07afa45cfaae45326f907
Kevin Sanchez <erik18012@gmail.com>
2025-01-07 19:03:51 -0300

    Agregados metodos POST y GET en equipos

--- website/views.py
+++ website/views.py
@@ -1,13 +1,25 @@
 from flask import Blueprint, request, render_template
 from .models import Equipo, Jugador
+from datetime import date
+from . import db
+
 
 views = Blueprint("views", __name__)
 
-def validar_equipo(equipo):
-    if equipo.divison not in ["Sub-18", "Sub-21", "Mayores"]:
+def validar_division_equipo(division):
+    if division not in ["Sub-18", "Sub-21", "Mayores", ""]:
         return False
     return True
 
+def validar_nombre(nombre):
+    if len(nombre) < 2:
+        return False
+    return True
+def validar_fecha(fecha):
+    # TO DO
+    # No permitir fechas posteriores al dia actual
+    return True
+    
 def validar_jugador(jugador):
     if len(jugador.dni) < 7:
         return False
@@ -21,13 +33,32 @@ def home():
 def equipos():
     #funcion que muestra todos los equipos
     if request.method == "GET":
-        #Equipo.query.all()
+        equipos = Equipo.query.order_by(Equipo.nombre).all()
+        if len(equipos) > 0:
+            for equipo in equipos:
+                print(f"Nombre de equipo: {equipo.nombre}")
+                print(f"Fecha de ingreso: {equipo.fecha_ingreso}")
+                print(f"DNI de entrenador: {equipo.entrenador_id}")
+                # A pesar de agregar un entrenador, este no aparece => entrenador_id es una clave foranea
+        else:
+            print("No hay equipos")
         return "<h1>Todos los equipos!</h1>"
-    else:
-        equipo = request.form.get("equipo")
-        if validar_equipo(equipo=equipo) == True:
-            #
+    elif request.method == "POST":
+        #Levantar valores del front
+        nombre = request.form.get("nombre")
+        contacto = request.form.get("contacto")
+        division = request.form.get("division")
+        fecha_ingreso = date.fromisoformat(request.form.get("fecha_ingreso")) #convertir string en un objeto date
+        entrenador_id = request.form.get("entrenador")
+        #Validar valores
+        if validar_division_equipo(division) and validar_nombre(nombre) and validar_fecha(fecha_ingreso):
+            #agregar equipo a la bd
+            nuevo_equipo = Equipo(nombre=nombre, fecha_ingreso=fecha_ingreso,contacto=contacto, division=division,entrenador_id=entrenador_id)
+            db.session.add(nuevo_equipo)
+            db.session.commit()
             return "POST REQUEST"
+        else:
+            return "ERROR EN LOS PARAMETROS"
 
 @views.route("/jugadores")
 def jugadores():
