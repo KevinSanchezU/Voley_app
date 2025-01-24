90c7901817e6f16600e914a3ac45c2e204b9228e
Facu Vera <fakundoo2010@gmail.com>
2025-01-23 23:19:46 -0300

    changes

---
+++ front/js/main.js
@@ -0,0 +1,16 @@
+const openModal = document.querySelector('.modal-on');
+const modal = document.querySelector('.modal-inscripcion');
+const closeModal = document.querySelector('.modal-off');
+
+let equipos = [];
+
+
+openModal.addEventListener('click', (e)=>{
+    e.preventDefault();
+    modal.classList.add('modal-show');
+})
+
+closeModal.addEventListener('click', (e)=>{
+    e.preventDefault();
+    modal.classList.remove('modal-show');
+})
